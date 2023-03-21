6413a13c3041915efd704368b7eaee81cb469d7a
shyu216 <79613808+shyu216@users.noreply.github.com>
2023-03-21 21:17:37 +0800

    editdataprocessortofitspconvv2

--- pcdet/datasets/processor/data_processor.py
+++ pcdet/datasets/processor/data_processor.py
@@ -254,16 +254,43 @@ class DataProcessor(object):
 
     def transform_points_to_voxels_valid(self, data_dict=None, config=None, voxel_generator=None):
         if data_dict is None:
+            # try:
+            #     from spconv.utils import VoxelGeneratorV2 as VoxelGenerator
+            # except:
+            #     from spconv.utils import VoxelGenerator
+
             try:
                 from spconv.utils import VoxelGeneratorV2 as VoxelGenerator
+                self.spconv_ver = 1
             except:
-                from spconv.utils import VoxelGenerator
-            voxel_generator = VoxelGenerator(
-                voxel_size=config.VOXEL_SIZE,
-                point_cloud_range=self.point_cloud_range,
-                max_num_points=config.MAX_POINTS_PER_VOXEL,
-                max_voxels=config.MAX_NUMBER_OF_VOXELS[self.mode]
-            )
+                try:
+                    from spconv.utils import VoxelGenerator
+                    self.spconv_ver = 1
+                except:
+                    from spconv.utils import Point2VoxelCPU3d as VoxelGenerator
+                    self.spconv_ver = 2
+
+            # voxel_generator = VoxelGenerator(
+            #     voxel_size=config.VOXEL_SIZE,
+            #     point_cloud_range=self.point_cloud_range,
+            #     max_num_points=config.MAX_POINTS_PER_VOXEL,
+            #     max_voxels=config.MAX_NUMBER_OF_VOXELS[self.mode]
+            # )
+            if self.spconv_ver == 1:
+                voxel_generator = VoxelGenerator(
+                    voxel_size=config.VOXEL_SIZE,
+                    point_cloud_range=self.point_cloud_range,
+                    max_num_points=config.MAX_POINTS_PER_VOXEL,
+                    max_voxels=config.MAX_NUMBER_OF_VOXELS[self.mode]
+                )
+            else:
+                voxel_generator = VoxelGenerator(
+                    vsize_xyz=config.VOXEL_SIZE,
+                    coors_range_xyz=self.point_cloud_range,
+                    num_point_features=self.num_point_features,
+                    max_num_points_per_voxel=config.MAX_POINTS_PER_VOXEL,
+                    max_num_voxels=config.MAX_NUMBER_OF_VOXELS[self.mode]
+                )
             grid_size = (self.point_cloud_range[3:6] - self.point_cloud_range[0:3]) / np.array(config.VOXEL_SIZE)
             self.grid_size = np.round(grid_size).astype(np.int64)
             self.voxel_size = config.VOXEL_SIZE
@@ -286,12 +313,27 @@ class DataProcessor(object):
             data_dict['voxel_num_points'] = num_points
         else:
             points = data_dict['points']
-            voxel_output = voxel_generator.generate(points)
-            if isinstance(voxel_output, dict):
-                voxels, coordinates, num_points = \
-                    voxel_output['voxels'], voxel_output['coordinates'], voxel_output['num_points_per_voxel']
+            # voxel_output = voxel_generator.generate(points)
+            # if isinstance(voxel_output, dict):
+            #     voxels, coordinates, num_points = \
+            #         voxel_output['voxels'], voxel_output['coordinates'], voxel_output['num_points_per_voxel']
+            # else:
+            #     voxels, coordinates, num_points = voxel_output
+            if self.spconv_ver == 1:
+                voxel_output = voxel_generator.generate(points)
+                if isinstance(voxel_output, dict):
+                    voxels, coordinates, num_points = \
+                        voxel_output['voxels'], voxel_output['coordinates'], voxel_output['num_points_per_voxel']
+                else:
+                    voxels, coordinates, num_points = voxel_output
             else:
-                voxels, coordinates, num_points = voxel_output
+                assert tv is not None, f"Unexpected error, library: 'cumm' wasn't imported properly."
+                voxel_output = voxel_generator.point_to_voxel(tv.from_numpy(points))
+                tv_voxels, tv_coordinates, tv_num_points = voxel_output
+                # make copy with numpy(), since numpy_view() will disappear as soon as the generator is deleted
+                voxels = tv_voxels.numpy()
+                coordinates = tv_coordinates.numpy()
+                num_points = tv_num_points.numpy()
 
             if not data_dict['use_lead_xyz']:
                 voxels = voxels[..., 3:]  # remove xyz in voxels(N, 3)
